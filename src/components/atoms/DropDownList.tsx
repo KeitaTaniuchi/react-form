e479745832d1a944626b3e9a4ef06567a7a7d0aa
k.taniuchi <barbeekonta1995@gmail.com>
2022-04-26 21:35:03 +0900

    React Selectをアンインストール

--- src/components/atoms/DropDownList.tsx
+++ src/components/atoms/DropDownList.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import DropDownListType from 'types/dropDownList';
-import Select from 'react-select';
 
 type Props = {
   placeHolder: string;
@@ -12,16 +11,38 @@ type Props = {
 const DropDownList: React.FC<Props> = ({ placeHolder, options, name, setDropDownValue }) => {
   return (
     <form>
-      <Select
-        placeholder={placeHolder}
+      <select
         name={name}
-        options={options}
-        onChange={(option: /* NonNullable<DropDownListType> */ any) => {
-          /* 型エラーを解消するために、ライブラリ名「react-select」の型定義を一部変更しています */
-          /* (変更した型定義の定義場所) /node_modules/react-select/dist/declarations/src/types.d.ts */
-          setDropDownValue(option.value);
+        onChange={(e: React.FormEvent<HTMLSelectElement>) => {
+          //「e.currentTarget.value」の型がstringのため、number型に変換
+          //なぜstring型なのかは不明
+          setDropDownValue(Number(e.currentTarget.value));
         }}
-      />
+        className='form-select appearance-none
+      block
+      w-full
+      px-4
+      py-2
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding bg-no-repeat
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+      >
+        <option disabled selected>
+          {placeHolder}
+        </option>
+        {options.map((option, index) => (
+          <option key={index} value={option.value} placeholder={placeHolder}>
+            {option.label}
+          </option>
+        ))}
+      </select>
     </form>
   );
 };
